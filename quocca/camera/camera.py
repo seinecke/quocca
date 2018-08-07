cbf92bf70b7f122de9a732c318dd251a3568b626
Tobias Hoinka <thoinka@Tobiass-Mac-mini.local>
2018-08-07 16:03:16 +0200

    Added docstrings for camera.py

--- quocca/camera/camera.py
+++ quocca/camera/camera.py
@@ -18,11 +18,38 @@ from astropy.coordinates import Angle, EarthLocation
 
 
 class Camera:
+    """Camera class, contains parameters specific to a camera.
+    
+    Attributes
+    ----------
+    az_offset : astropy.coordinates.Angle object
+    	Offset angle against north in degrees.
+    location : astropy.coordinates.EarthLocation object
+    	Location of the camera.
+    mask : scipy.interpolate.RegularGridInterpolator
+    	Camera-specific mask of permanently obstured objects in field of view.
+    name : str
+    	Name of the camera.
+    
+	Notes
+	-----
+	Supported cameras are gathered from a config file located in
+	`resources/cameras.yaml`. Adding, removing or altering the parameters
+	of cameras requires editing that config manually.
+    """
+
     with open(resource_filename('quocca', 'resources/cameras.yaml')) as file:
         __config__ = yaml.safe_load(file)
         __supported_cameras__ = list(__config__.keys())
     
     def __init__(self, name):
+        """Constructor.
+        
+        Parameters
+        ----------
+        name : str
+            Name of the character.
+        """
         if name not in self.__supported_cameras__:
             raise NotImplementedError('Unsupported Camera {}'.format(name))
         else:
@@ -42,6 +69,20 @@ class Camera:
         return 'Camera {}'.format(self.name)
     
     def theta2r(self, theta):
+        """Converts an altitude `theta` into a apparent radius in pixels. Used
+        function depends on attribute :attribute:`mapping`. Currently, only
+        `lin` and `nonlin` are supported.
+        
+        Parameters
+        ----------
+        theta : astropy.coordinates.Angle
+            Altitude angle in degrees.
+        
+        Returns
+        -------
+        r : numpy.array
+            Calculated radii
+        """
         if self.mapping == 'lin':
             return self.radius / (np.pi * u.rad / 2.0) * theta.to(u.rad)
         elif self.mapping == 'nonlin':
@@ -51,15 +92,61 @@ class Camera:
             	                      .format(self.mapping))
         
     def r2theta(self, r):
+    	"""Converts a radius in pixels `r` into an altitude. Used function
+    	depends on attribute :attribute:`mapping`. Currently, only `lin` and
+    	`nonlin` are supported.
+        
+        Parameters
+        ----------
+        r : numpy.array
+            Radius in pixels.
+        
+        Returns
+        -------
+        theta : numpy.array
+            Calculated altitude in degrees
+        """
         if self.mapping == 'lin':
             return r * (np.pi * u.rad) / (2.0 * self.radius)
         elif self.mapping == 'nonlin':
             return 2.0 * np.arcsin(np.sqrt(2.0) * r / self.radius)
         
     def check_mask(self, x, y):
-        return self.mask((x, y))
+        """Checks whether or not within the bounds of the mask provided. True
+        means the checked point may not be obstructed by an permanently present
+        object.
+        
+        Parameters
+        ----------
+        x : numpy.array
+            x coordinate in pixels.
+        y : numpy.array
+            y coordinate in pixels.
+        
+        Returns
+        -------
+        numpy.array, dtype=bool
+            Whether or not the points are obstructed.
+        """
+        return self.mask((x, y)) == 1
         
     def project_stars(self, catalog, time):
+        """Projects stars listed in `catalog` onto the camera at `time`.
+        
+        Parameters
+        ----------
+        catalog : quocca.catalog.Catalog object
+            The catalog containing the stars to be projected.
+        time : astropy.time.Time object
+            Timestamp.
+        
+        Returns
+        -------
+        pixel_coordinates : numpy.array, shape=(n_stars, 2)
+            Calculated pixel coordinates for each star.
+        magnitude : numpy.array
+        	Magnitude of each star.
+        """
         altaz = catalog.get_horizontal(self, time)
         phi, theta = altaz.az, altaz.alt
         r = self.theta2r(Angle('90d') - theta)
