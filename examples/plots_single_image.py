152b6c991983b30597e4bf80b24734e4f7e015e6
Sabrina Einecke <sabrina.einecke@uni-dortmund.de>
2018-08-23 15:09:23 +0930

    Description of example script and naming changes

--- examples/plots_single_image.py
+++ examples/plots_single_image.py
@@ -1,6 +1,11 @@
 """
 Example of creating the following plots for a single image:
+- Simple skymap with coordinate system
 - Visibility values for each star on a skymap
+- Comparison between fitted and true star positions on a skymap
+- Comparison between serlected stars for fit and all catalog stars on a skymap
+- 2D histogram of estimated and true magnitude
+- 2D histogram of visibility and true magnitude
 
 Usage: 
 --------
@@ -21,6 +26,7 @@ from quocca.detection import StarDetectionLLH
 from quocca.plotting import skymap_visibility, compare_used_stars_to_catalog
 from quocca.plotting import compare_fitted_to_true_positions
 from quocca.plotting import compare_estimated_to_true_magnitude
+from quocca.plotting import compare_visibility_to_magnitude
 
 
 
@@ -42,21 +48,30 @@ if __name__ == '__main__':
     det = StarDetectionLLH(cam)
     res = det.detect(img, max_mag=20)
 
+    ax = img.show()
+    plt.tight_layout()
+    plt.savefig(outdir + name + '_skymap.pdf')
+
     ax = skymap_visibility(img, res, max_mag=7)
     plt.tight_layout()
     plt.savefig(outdir + name + '_skymap-visibility.pdf')
 
     ax = compare_used_stars_to_catalog(img, res, max_mag=7)
     plt.tight_layout()
-    plt.savefig(name+'_skymap-used_stars_to_catalog.pdf')
+    plt.savefig(outdir + name + '_skymap-used_stars_and_catalog.pdf')
 
     ax = compare_fitted_to_true_positions(img, res, max_mag=7)
     plt.tight_layout()
-    plt.savefig(outdir + name + '_skymap-fitted_to_true_positions.pdf')
-    plt.show()
+    plt.savefig(outdir + name + '_skymap-fitted_and_true_positions.pdf')
 
     ax = compare_estimated_to_true_magnitude(res, det)
     plt.tight_layout()
-    plt.savefig(outdir + name + '_skymap-estimated_to_true_magnitude.pdf')
+    plt.savefig(outdir + name + '_hist-estimated_and_true_magnitude.pdf')
+
+    ax = compare_visibility_to_magnitude(res)
+    plt.tight_layout()
+    plt.savefig(outdir + name + '_hist-true_magnitude_and_visibility.pdf')
+
+
 
 
