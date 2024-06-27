f24f6d263d5eb1d29d5f7be23858a80e40245626
Sabrina Einecke <sabrina.einecke@cta-consortium.org>
2024-06-27 16:32:19 +0930

    New LLH detection settings
    LLH fit is now setup for different purposes, for example:
    - to determine psf, the sigma is fitted
    - to determine psf asymmetry, sigx/sigy/correlation is fitted
    - to calibrate, the start value is on the brightest spot instead of the star position

--- quocca/detection/detection.py
+++ quocca/detection/detection.py
@@ -185,7 +185,36 @@ def blob_func(x, y, x0, y0, mag, sigma, bkg):
         Model.
     """
     arg = -((x - x0) ** 2 + (y - y0) ** 2) / (2.0 * sigma ** 2)
-    return np.abs(mag) * np.exp(arg) + np.abs(bkg)
+    return np.clip(np.abs(mag) * np.exp(arg) + np.abs(bkg), 0, 1)
+
+
+def blob_bivar_func(x, y, x0, y0, mag, sigx, sigy, cor, bkg):
+    """Bivariate Gaussian model for the star.
+
+    Parameters
+    ----------
+    x, y : numpy.array
+        Coordinates for evaluation
+    x0, y0 : float
+        Location of the star.
+    mag : float
+        Magnitude of the star.
+    sigma : float
+        Spread of the star.
+    bkg : float
+        Background level.
+
+    Returns
+    -------
+    model : np.array
+        Model.    
+    """
+
+    arg = -( (1 / (2 * (1 - cor**2)) ) * \
+        ( ( (x - x0) / sigx)**2 
+            - 2 * cor * ( ( (x - x0) / sigx) * ( (y - y0) / sigy))
+            +( (y - y0) / sigy)**2) )
+    return np.clip(np.abs(mag) * np.exp(arg) + np.abs(bkg), 0, 1)
 
 
 class StarDetectionBase:
@@ -224,7 +253,8 @@ class StarDetectionLLH(StarDetectionBase):
     name = 'llh_star_detection'
 
     def __init__(self, camera, sigma=1.6, fit_size=4, presmoothing=0.0,
-                 remove_detected_stars=True, verbose=True, tol=1e-15):
+                 remove_detected_stars=True, fit_aim='detect', 
+                 verbose=True, tol=1e-15):
         super(StarDetectionLLH, self).__init__(camera)
         self.sigma = sigma
         self.size = (fit_size, fit_size)
@@ -232,6 +262,7 @@ class StarDetectionLLH(StarDetectionBase):
         self.verbose = verbose
         self.remove_detected_stars = remove_detected_stars
         self.tol = tol
+        self.fit_aim = fit_aim
     
     def detect(self, image):
         """Detect method.
@@ -272,7 +303,234 @@ class StarDetectionLLH(StarDetectionBase):
             'b_fit',
             'x_fit',
             'y_fit',
+            #'sig_fit',
+            #'sigx_fit',
+            #'sigy_fit',
+            #'cor_fit',
+            'success_fit',
+            'llh_fit',
+            'iter_fit',
+            'saturated',
             'visibility'
+        ]
+        results = {key: np.zeros(n_stars) for key in keys}
+
+        # Sort by magnitude to process stars ordered by magnitude.
+        mag_sort_idx = np.argsort(image.stars.mag.values)
+        if self.verbose:
+            iterator = tqdm(mag_sort_idx, total=len(mag_sort_idx))
+        else:
+            iterator = mag_sort_idx
+        for idx in iterator:
+            sel = crop((pos[idx,1], pos[idx,0]), self.size, img.shape)
+            sel_max = np.max(img[sel])
+            sel_mean = np.mean(img[sel])
+            xy = np.unravel_index(np.argmax(img[sel]), img[sel].shape)
+
+            if self.fit_aim == 'detect':
+                def fit_function(p):
+                    return np.sum((
+                        blob_func(mx[sel], my[sel], p[0], p[1], p[2],
+                                  self.sigma, p[3]) - img[sel]) ** 2
+                        / (img[sel] + 1e-8)
+                    )
+                x0 = [pos[idx,1], pos[idx,0], 
+                      sel_max - sel_mean, sel_mean]
+                bounds = (
+                    (pos[idx,1]-self.size[1], pos[idx,1]+self.size[1]),
+                    (pos[idx,0]-self.size[0], pos[idx,0]+self.size[0]),
+                    (0.0, 2*sel_max),
+                    (0.0, sel_max),
+                    )
+            elif self.fit_aim == 'calib':
+                def fit_function(p):
+                    return np.sum((
+                        blob_func(mx[sel], my[sel], p[0], p[1], p[2],
+                                  self.sigma, p[3]) - img[sel]) ** 2
+                        / (img[sel] + 1e-8)
+                    )
+                x0 = [mx[sel][xy], my[sel][xy],
+                      sel_max - sel_mean, sel_mean]
+                bounds = (
+                    (pos[idx,1]-self.size[1], pos[idx,1]+self.size[1]),
+                    (pos[idx,0]-self.size[0], pos[idx,0]+self.size[0]),
+                    (0.0, 2*sel_max),
+                    (0.0, sel_max),
+                    )
+            elif self.fit_aim == 'psf':
+                def fit_function(p):
+                    return np.sum((
+                        blob_func(mx[sel], my[sel], p[0], p[1], p[2],
+                                  p[4], p[3]) - img[sel]) ** 2
+                        / (img[sel] + 1e-8)
+                    )
+                x0 = [mx[sel][xy], my[sel][xy],
+                      sel_max - sel_mean, sel_mean,
+                      self.sigma]
+                bounds = (
+                    (pos[idx,1]-self.size[1], pos[idx,1]+self.size[1]),
+                    (pos[idx,0]-self.size[0], pos[idx,0]+self.size[0]),
+                    (0.0, 2*sel_max),
+                    (0.0, sel_max),
+                    (0.1*self.sigma, 2*self.sigma)
+                    )
+            elif self.fit_aim == 'asym':
+                def fit_function(p):
+                    return np.sum((
+                        blob_bivar_func(mx[sel], my[sel], p[0], p[1], p[2],
+                                  p[4], p[5], p[6], p[3]) - img[sel]) ** 2
+                        / (img[sel] + 1e-8)
+                    )
+                x0 = [mx[sel][xy], my[sel][xy],
+                      sel_max - sel_mean, sel_mean,
+                      self.sigma, self.sigma, 0.0]
+                bounds = (
+                    (pos[idx,1]-self.size[1], pos[idx,1]+self.size[1]),
+                    (pos[idx,0]-self.size[0], pos[idx,0]+self.size[0]),
+                    (0.0, 2*sel_max),
+                    (0.0, sel_max),
+                    (0.1*self.sigma, 2*self.sigma),
+                    (0.1*self.sigma, 2*self.sigma),
+                    (-1.0, 1.0)
+                    )
+
+            # Optimization details:
+            # 1. max - mean is a good starting value for M
+            # 2. mean is a good starting value for b
+            # 3. The true x and y coordinates in the
+            #    cropped image are good starting values for x0 and y0.
+            # 4. SLSQP is by far the fastest minimzation method for this task
+            #    plus it's more accurate, plus it can even handle bounds.
+            r = minimize(
+                fit_function,
+                x0=x0,
+                method='SLSQP',
+                options={'maxiter': 500},
+                tol=self.tol,
+                bounds=bounds
+            )
+
+            if (self.fit_aim == 'detect') | (self.fit_aim == 'calib'):
+                if self.remove_detected_stars:
+                    img[sel] = img[sel] - blob_func(mx[sel], my[sel], r.x[0], r.x[1],
+                                          r.x[2], self.sigma, r.x[3]) + r.x[3]
+            elif self.fit_aim == 'psf':
+                if self.remove_detected_stars:
+                    img[sel] = img[sel] - blob_func(mx[sel], my[sel], r.x[0], r.x[1],
+                                          r.x[2], r.x[4], r.x[3]) + r.x[3]
+                    if 'sig_fit' not in results.keys():
+                        results['sig_fit'] = np.zeros(n_stars)
+                    results['sig_fit'][idx] = r.x[4]
+            elif self.fit_aim == 'asym':
+                if self.remove_detected_stars:
+                    img[sel] = img[sel] - blob_bivar_func(mx[sel], my[sel], 
+                        r.x[0], r.x[1], r.x[2], r.x[4], r.x[5], r.x[6], r.x[3]
+                        ) + r.x[3]
+                    if 'sigx_fit' not in results.keys():
+                        results['sigx_fit'] = np.zeros(n_stars)
+                        results['sigy_fit'] = np.zeros(n_stars)
+                        results['cor_fit'] = np.zeros(n_stars)
+                    results['sigx_fit'][idx] = r.x[4]
+                    results['sigy_fit'][idx] = r.x[5]
+                    results['cor_fit'][idx] = r.x[6]
+
+            
+            visibility = np.abs(r.x[0]) / np.exp(-image.stars.mag.iloc[idx])
+            results['id'][idx] = image.stars.id.iloc[idx]
+            results['M_fit'][idx] = np.abs(r.x[2])
+            results['visibility'][idx] = visibility * self.calibration
+            results['b_fit'][idx] = np.abs(r.x[3])
+            results['y_fit'][idx] = r.x[0]
+            results['x_fit'][idx] = r.x[1]
+            results['llh_fit'][idx] = r.fun
+            results['success_fit'][idx] = r.success
+            results['iter_fit'][idx] = r.nit
+            if sel_max == 1.0:
+                results['saturated'][idx] = True
+            else:
+                results['saturated'][idx] = False
+        results = pd.DataFrame(results, index=results['id'].astype(int))
+        results.id = results.id.astype(int)
+        return results
+
+
+class StarDetectionLLH2(StarDetectionBase):
+    """Likelihood approach to star detection. Fits every star individually
+    using a more complex model than 'StarDetectionLLH'.
+    
+    Attributes
+    ----------
+    name : str
+        Name of the method
+    presmoothing : float
+        Sigma parameter of the Gaussian filter applied beforehand.
+    sigma : float
+        Sigma parameter of the model used during fit.
+    size : tuple(int)
+        Size of the cropped out parts of the image for fitting.
+    verbose : bool
+        Verbosity flag.
+    remove_detected_stars : bool
+        Whether or not to successively remove fitted stars as they are
+        detected.
+    """
+    name = 'llh2_star_detection'
+
+    def __init__(self, camera, sigma=1.6, fit_size=4, presmoothing=0.0,
+                 remove_detected_stars=True, verbose=True, tol=1e-15):
+        super(StarDetectionLLH2, self).__init__(camera)
+        self.sigma = sigma
+        self.size = (fit_size, fit_size)
+        self.presmoothing = presmoothing
+        self.verbose = verbose
+        self.remove_detected_stars = remove_detected_stars
+        self.tol = tol
+    
+    def detect(self, image):
+        """Detect method.
+        
+        Parameters
+        ----------
+        image : quocca.image.Image
+            Image.
+        
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame containing all the results, namely
+                * `id`: Id of each star.
+                * `M_fit`: Fitted linear magnitude.
+                * `b_fit`: Fitted background level.
+                * `x_fit`: Fitted x-position.
+                * `y_fit`: Fitted y-position.
+                * `visibility`: Calculated visibility factor.
+        """
+        super(StarDetectionLLH2, self).detect(image)
+
+        # Apply presmoothing.
+        img = gaussian(image.image, self.presmoothing)
+        
+        # Generate meshgrid for pixel coordinate grid.
+        tx = np.arange(img.shape[0])
+        ty = np.arange(img.shape[1])
+        mx, my = np.meshgrid(ty, tx)
+        n_stars = len(image.stars)
+        pos = np.column_stack((image.stars.x.values,
+                               image.stars.y.values))
+
+        # Prepare keys to write during the detection.
+        keys = [
+            'id',
+            'M_fit',
+            'b_fit',
+            'x_fit',
+            'y_fit',
+            'sigx_fit',
+            'sigy_fit',
+            'cor_fit',
+            'success_fit',
+            'llh_fit',
+            'visibility',
         ]
         results = {key: np.zeros(n_stars) for key in keys}
 
@@ -286,8 +544,9 @@ class StarDetectionLLH(StarDetectionBase):
             sel = crop((pos[idx,1], pos[idx,0]), self.size, img.shape)
             def fit_function(p):
                 return np.sum((
-                    blob_func(mx[sel], my[sel], p[2], p[3], p[0],
-                              self.sigma, p[1]) - img[sel]) ** 2
+                    blob_bivar_func(mx[sel], my[sel], 
+                        p[0], p[1], p[2], p[3], p[4], p[5], p[6]
+                        ) - img[sel]) ** 2
                     / (img[sel] + 1e-8)
                 )
             # Optimization details:
@@ -302,27 +561,37 @@ class StarDetectionLLH(StarDetectionBase):
             sel_mean = np.mean(img[sel])
             r = minimize(
                 fit_function,
-                x0=[sel_max - sel_mean, sel_mean, #mean[0], mean[1]],
-                pos[idx,1], pos[idx,0]],
+                x0=[pos[idx,1], pos[idx,0], sel_max - sel_mean, 
+                    self.sigma, self.sigma, 0, sel_mean,
+                    ],
                 method='SLSQP',
                 tol=self.tol,
                 bounds=(
-                    (0.0, sel_max),
-                    (0.0, sel_max),
                     (pos[idx,1]-self.size[1], pos[idx,1]+self.size[1]),
-                    (pos[idx,0]-self.size[0], pos[idx,0]+self.size[0])
+                    (pos[idx,0]-self.size[0], pos[idx,0]+self.size[0]),
+                    (0.0, sel_max),
+                    (0.1*self.sigma, 2*self.sigma),
+                    (0.1*self.sigma, 2*self.sigma),
+                    (-1, 1),
+                    (0.0, sel_max),  
                 )
             )
             if self.remove_detected_stars:
-                img[sel] -= blob_func(mx[sel], my[sel], r.x[2], r.x[3],
-                                      r.x[0], self.sigma, 0.0)
-            visibility = np.abs(r.x[0]) / np.exp(-image.stars.mag.iloc[idx])
+                img[sel] -= blob_bivar_func(mx[sel], my[sel], 
+                            r.x[0], r.x[1], r.x[2], r.x[3],
+                            r.x[4], r.x[5], r.x[6],)
+            visibility = np.abs(r.x[2]) / np.exp(-image.stars.mag.iloc[idx])
             results['id'][idx] = image.stars.id.iloc[idx]
-            results['M_fit'][idx] = np.abs(r.x[0])
+            results['M_fit'][idx] = np.abs(r.x[2])
             results['visibility'][idx] = visibility * self.calibration
-            results['b_fit'][idx] = np.abs(r.x[1])
-            results['y_fit'][idx] = r.x[2]
-            results['x_fit'][idx] = r.x[3]
+            results['b_fit'][idx] = np.abs(r.x[6])
+            results['y_fit'][idx] = r.x[0]
+            results['x_fit'][idx] = r.x[1]
+            results['sigy_fit'][idx] = r.x[3]
+            results['sigx_fit'][idx] = r.x[4]
+            results['cor_fit'][idx] = r.x[5]
+            results['success_fit'][idx] = r.success
+            results['llh_fit'][idx] = r.fun
         results = pd.DataFrame(results, index=results['id'].astype(int))
         results.id = results.id.astype(int)
         return results
